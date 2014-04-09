4d5819c60f45baad86ddfe8fc6150b6bb4181424
Robert Carlsen <rwcarlsen@gmail.com>
2014-04-09 15:17:57 -0500

    added separator facility

---
+++ agents/separator.cc
@@ -0,0 +1,86 @@
+#include "separator.h"
+
+#define LG(X) LOG(cyclus::LEV_##X, "Seprat")
+
+using cyclus::MatQuery;
+using cyclus::Material;
+using cyclus::CompMap;
+using cyclus::Composition;
+using cyclus::ResCast;
+using cyclus::Nuc;
+using cyclus::compmath::Normalize;
+
+Separator::Separator(cyclus::Context* ctx)
+    : cyclus::Facility(ctx),
+      inbuf_size_(0),
+      outbuf_size_(0),
+      wastebuf_size_(0),
+      throughput_(0),
+      inpolicy_(this),
+      outpolicy_(this),
+      wastepolicy_(this) {}
+
+void Separator::DoRegistration() {
+  cyclus::Facility::DoRegistration();
+
+  outpolicy_.Init(&outbuf_, outcommod_);
+  wastepolicy_.Init(&wastebuf_, wastecommod_);
+  inpolicy_.Init(&inbuf_, incommod_, context()->GetRecipe(inrecipe_));
+
+  context()->RegisterTrader(&outpolicy_);
+  context()->RegisterTrader(&wastepolicy_);
+  context()->RegisterTrader(&inpolicy_);
+}
+
+void Separator::Tick(int time) {
+  if (inbuf_.count() == 0) {
+    return;
+  }
+
+  LG(INFO3) << "Separator id=" << id() << " is ticking";
+  LG(INFO4) << "inbuf quantity = " << inbuf_.quantity();
+  LG(INFO4) << "outbuf quantity = " << outbuf_.quantity();
+  LG(INFO4) << "wastebuf quantity = " << wastebuf_.quantity();
+
+  std::vector<Material::Ptr> mats;
+  mats = ResCast<Material>(inbuf_.PopN(inbuf_.count()));
+  for (int i = 1; i < mats.size(); ++i) {
+    mats[0]->Absorb(mats[i]);
+  }
+  Material::Ptr m = mats[0];
+
+  CompMap cm = m->comp()->mass();
+  Normalize(&cm, m->quantity());
+  CompMap::iterator it;
+  double qty = 0;
+  for (it = cm.begin(); it != cm.end(); ++it) {
+    Nuc n = it->first;
+    if (nucs_.count(n) == 0) {
+      cm.erase(n);
+    } else {
+      cm[n] *= effs_[n];
+      qty += cm[n];
+    }
+  }
+  Composition::Ptr c = Composition::CreateFromMass(cm);
+  Material::Ptr keep = m->ExtractComp(qty, c);
+
+  if (m->quantity() <= wastebuf_.space()) {
+    wastebuf_.Push(m);
+  } else {
+    wastebuf_.Push(m->ExtractQty(wastebuf_.space()));
+    inbuf_.Push(m);
+  }
+
+  if (qty < throughput_ && qty < outbuf_.space()) {
+    outbuf_.Push(keep);
+  } else {
+    qty = std::min(throughput_, outbuf_.space());
+    outbuf_.Push(keep->ExtractQty(qty));
+    inbuf_.Push(keep);
+  }
+}
+
+extern "C" cyclus::Agent* ConstructSeparator(cyclus::Context* ctx) {
+  return new Separator(ctx);
+}
---
+++ agents/separator.h
@@ -0,0 +1,59 @@
+#ifndef SEPARATOR_H_
+#define SEPARATOR_H_
+
+#include <string>
+#include "cyclus.h"
+#include "sell_policy.h"
+#include "buy_policy.h"
+
+class Separator : public cyclus::Facility {
+ public:
+  Separator(cyclus::Context* ctx);
+  virtual ~Separator() {};
+  virtual std::string str() {return "";};
+
+  virtual void DoRegistration();
+
+  #pragma cyclus
+
+  virtual void Tick(int time);
+  virtual void Tock(int time) {};
+
+ private:
+  #pragma cyclus var {}
+  std::string incommod_;
+  #pragma cyclus var {}
+  std::string inrecipe_;
+  #pragma cyclus var {}
+  double inbuf_size_;
+  #pragma cyclus var {'capacity': 'inbuf_size_'}
+  cyclus::ResourceBuff inbuf_;
+
+  #pragma cyclus var {}
+  std::string outcommod_;
+  #pragma cyclus var {}
+  double outbuf_size_;
+  #pragma cyclus var {'capacity': 'outbuf_size_'}
+  cyclus::ResourceBuff outbuf_;
+
+  #pragma cyclus var {}
+  std::string wastecommod_;
+  #pragma cyclus var {}
+  double wastebuf_size_;
+  #pragma cyclus var {'capacity': 'wastebuf_size_'}
+  cyclus::ResourceBuff wastebuf_;
+
+  #pragma cyclus var {}
+  std::set<int> nucs_;
+  #pragma cyclus var {}
+  std::map<int, double> effs_;
+
+  #pragma cyclus var {}
+  double throughput_;
+
+  SellPolicy outpolicy_;
+  SellPolicy wastepolicy_;
+  BuyPolicy inpolicy_;
+};
+
+#endif  // RECIPE_EXTRACTOR_H_
