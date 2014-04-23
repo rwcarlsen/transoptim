ff7ed8ad07e04497eddc2d70f14c8076979325e0
Robert Carlsen <rwcarlsen@gmail.com>
2014-04-23 16:17:10 -0500

    reduce upper bound of support facs

--- dakota-exp/scenario.go
+++ dakota-exp/scenario.go
@@ -110,7 +110,7 @@ func (s *Scenario) UpperBounds() *mat64.Dense {
 			if fac.Cap != 0 {
 				up.Set(f*nperiods+n, 0, v*2)
 			} else {
-				up.Set(f*nperiods+n, 0, 100)
+				up.Set(f*nperiods+n, 0, 20)
 			}
 		}
 	}
